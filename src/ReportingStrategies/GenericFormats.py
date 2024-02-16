b5c7cc50ef23d3d18e0405cea55ca4718a3a66b9
MarjanDB <mocnik.mario@hotmail.com>
2024-02-16 23:00:22 +0100

    New Generic Schemas

--- src/ReportingStrategies/GenericFormats.py
+++ src/ReportingStrategies/GenericFormats.py
@@ -2,6 +2,13 @@ from enum import Enum
 from dataclasses import dataclass
 from arrow import Arrow
 
+class GenericAssetClass(str, Enum):
+    STOCK = "STOCK"
+    ROYALTY_TRUST = "ROYALTY_TRUST"
+    CASH = "CASH"
+    OPTION = "OPTION"
+
+
 # https://www.racunovodstvo.net/zakonodaja/zdoh/90-clen
 class GenericDividendType(str, Enum):
     UNKNOWN = ""        # For internal use of this reporting script
@@ -61,11 +68,7 @@ class GenericTradeReportItemGainType(str, Enum):
     RIGHT_TO_NEWLY_ISSUED_STOCK = "RIGHT_TO_NEWLY_ISSUED_STOCK" # guessing
 
 
-class GenericAssetClass(str, Enum):
-    STOCK = "STOCK"
-    ROYALTY_TRUST = "ROYALTY_TRUST"
-    CASH = "CASH"
-    OPTION = "OPTION"
+
 
 @dataclass
 class GenericTradeReportItemSecurityLineBought:
@@ -108,3 +111,89 @@ class GenericTradeReportItem:
     ForeignTaxCountryName: str | None
 
     Lines: list[GenericTradeReportItemSecurityLineBought | GenericTradeReportItemSecurityLineSold]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class GenericDerivativeReportItemType(str, Enum):
+    DERIVATIVE = "DERIVATIVE"
+    DERIVATIVE_SHORT = "DERIVATIVE_SHORT"
+
+class GenericDerivativeReportAssetClassType(str, Enum):
+    FUTURES_CONTRACT = "FUTURES_CONTRACT" # https://www.investopedia.com/terms/f/futurescontract.asp
+    CONTRACT_FOR_DIFFERENCE = "CFD" # https://www.investopedia.com/terms/c/contractfordifferences.asp
+    OPTION = "OPTION"
+    CERTIFICATE = "CERTIFICATE"
+    OTHER = "OTHER"
+
+class GenericDerivativeReportItemGainType(str, Enum):
+    CAPITAL_INVESTMENT = "CAPITAL_INVESTMENT" # guessing
+    BOUGHT = "BOUGHT"
+    CAPITAL_RAISE = "CAPITAL_RAISE" # guessing
+    CAPITAL_ASSET = "CAPITAL_ASSET" # guessing
+    CAPITALIZATION_CHANGE = "CAPITALIZATION_CHANGE" # guessing
+    INHERITENCE = "INHERITENCE"
+    GIFT = "GIFT"
+    OTHER = "OTHER"
+
+
+
+
+@dataclass
+class GenericDerivativeReportItemSecurityLineBought:
+    AcquiredDate: Arrow
+    AcquiredHow: GenericDerivativeReportItemGainType
+    NumberOfUnits: float
+    AmountPerUnit: float
+    TotalAmountPaid: float  # to avoid rounding errors in case of % purchases
+    Leveraged: bool
+    TransactionID: str
+
+
+@dataclass
+class GenericDerivativeReportItemSecurityLineSold:
+    SoldDate: Arrow
+    NumberOfUnitsSold: float
+    AmountPerUnit: float
+    TotalAmountSoldFor: float
+    TransactionID: str
+    Leveraged: bool
+    WashSale: bool  # no trades 30 days before and after, and sold for loss
+    SoldForProfit: bool
+
+@dataclass
+class GenericDerivativeReportLotMatches:
+    TransactionID: str
+    Quantitiy: float
+    LotOriginalBuy: GenericDerivativeReportItemSecurityLineBought
+    LotOriginalSell: GenericDerivativeReportItemSecurityLineSold
+
+
+@dataclass
+class GenericDerivativeReportItem:
+    InventoryListType: GenericDerivativeReportItemType
+    InventoryAssetClass: GenericDerivativeReportAssetClassType
+    ISIN: str
+    Ticker: str
+    HasForeignTax: bool
+    ForeignTax: float | None
+    ForeignTaxCountryID: str | None
+    ForeignTaxCountryName: str | None
+
+    Lines: list[GenericDerivativeReportItemSecurityLineBought | GenericDerivativeReportItemSecurityLineSold]
