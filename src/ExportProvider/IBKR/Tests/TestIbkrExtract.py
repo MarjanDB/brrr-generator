1c49cf0f17d295fd5e916d66f7d363cd88bc51e4
MarjanDB <mocnik.mario@hotmail.com>
2024-03-23 16:31:18 +0100

    Tests for Option Extract

--- src/ExportProvider/IBKR/Tests/TestIbkrExtract.py
+++ src/ExportProvider/IBKR/Tests/TestIbkrExtract.py
@@ -5,24 +5,29 @@ import src.ExportProvider.IBKR.Extract as ex
 import src.ExportProvider.IBKR.Schemas as es
 
 locationOfFiles = pathlib.Path(__file__).parent
-singleTradeXml = os.path.join(locationOfFiles, "SingleTrade.xml")
+singleStockTradeXml = os.path.join(locationOfFiles, "SingleStockTrade.xml")
+singleOptionTradeXml = os.path.join(locationOfFiles, "SingleOptionTrade.xml")
 
-with open(singleTradeXml) as fobj:
+with open(singleStockTradeXml) as fobj:
     singleTradeString = fobj.read()
-    singleTrade: etree._Element = etree.fromstring(singleTradeString)
+    singleStockTrade: etree._Element = etree.fromstring(singleTradeString)
+
+with open(singleOptionTradeXml) as fobj:
+    singleTradeString = fobj.read()
+    singleOptionTrade: etree._Element = etree.fromstring(singleTradeString)
 
 
 
-class TestIbkrExtract:
+class TestIbkrExtractStockTrades:
     def testSegmentedTradesReturnTradesAndLot(self):
-        segmented = ex.extractFromXML(singleTrade)
+        segmented = ex.extractFromXML(singleStockTrade)
 
         assert isinstance(segmented, es.SegmentedTrades)
         assert len(segmented.stockTrades) == 2
         assert len(segmented.stockLots) == 1
 
     def testSegmentedTradesContainBuyAndSellEvent(self):
-        segmented = ex.extractFromXML(singleTrade)
+        segmented = ex.extractFromXML(singleStockTrade)
 
         buyTrade = segmented.stockTrades[0]
         assert buyTrade.BuyOrSell == es.BuyOrSell.BUY
@@ -37,13 +42,47 @@ class TestIbkrExtract:
         assert sellTrade.TransactionID == "262720557"
 
     def testBuyEventTransactionRelatesToLot(self):
-        segmented = ex.extractFromXML(singleTrade)
+        segmented = ex.extractFromXML(singleStockTrade)
 
         buyTrade = segmented.stockTrades[0]
         assert buyTrade.TransactionID == "241234985"
 
         lot = segmented.stockLots[0]
         assert lot.TransactionID == "241234985"
+
+class TestIbkrExtractOptionTrades:
+    def testSegmentedTradesReturnTradesAndLot(self):
+        segmented = ex.extractFromXML(singleOptionTrade)
+
+        assert isinstance(segmented, es.SegmentedTrades)
+        assert len(segmented.derivativeTrades) == 2
+        assert len(segmented.derivativeLots) == 1
+
+    def testSegmentedTradesContainBuyAndSellEvent(self):
+        segmented = ex.extractFromXML(singleOptionTrade)
+
+        buyTrade = segmented.derivativeTrades[0]
+        assert buyTrade.BuyOrSell == es.BuyOrSell.BUY
+        assert buyTrade.UnderlyingSecurityID == "US0378331005"
+        assert buyTrade.Quantity == 1
+        assert buyTrade.Multiplier == 100
+        assert buyTrade.TransactionID == "635331370"
+
+        sellTrade = segmented.derivativeTrades[1]
+        assert sellTrade.BuyOrSell == es.BuyOrSell.SELL
+        assert sellTrade.UnderlyingSecurityID == "US0378331005"
+        assert sellTrade.Quantity == -1
+        assert sellTrade.Multiplier == 100
+        assert sellTrade.TransactionID == "639309966"
+
+    def testBuyEventTransactionRelatesToLot(self):
+        segmented = ex.extractFromXML(singleOptionTrade)
+
+        buyTrade = segmented.derivativeTrades[0]
+        assert buyTrade.TransactionID == "635331370"
+
+        lot = segmented.derivativeLots[0]
+        assert lot.TransactionID == "635331370"
 
 
 
