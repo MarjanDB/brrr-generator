e3466e50ab0da538b212b3577633c720d8f85d66
MarjanDB <mocnik.mario@hotmail.com>
2024-05-01 15:34:53 +0200

    Move into core

---
+++ src/Core/StagingFinancialEvents/Schemas/Lots.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+from arrow import Arrow
+
+from Core.FinancialEvents.Schemas.CommonFormats import GenericShortLong
+
+
+@dataclass
+class StagingTaxLotMatchingDetails:
+    ID: str | None
+    DateTime: Arrow | None
+
+
+@dataclass
+class StagingTaxLot:
+    ID: str
+    ISIN: str
+    Ticker: str | None
+    Quantity: float
+    Acquired: StagingTaxLotMatchingDetails
+    Sold: StagingTaxLotMatchingDetails
+    ShortLongType: GenericShortLong  # Some trades can be SHORTING, meaning you first sell and then buy back
