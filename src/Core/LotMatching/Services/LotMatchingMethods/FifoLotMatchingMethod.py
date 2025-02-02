420b7b1aa51e0da673dcba2065804f97ad11067d
MarjanDB <mocnik.mario@hotmail.com>
2025-02-02 16:18:56 +0100

    fifo with buy/sell, buy buy / sell, buy / sell sell cases covered

--- src/Core/LotMatching/Services/LotMatchingMethods/FifoLotMatchingMethod.py
+++ src/Core/LotMatching/Services/LotMatchingMethods/FifoLotMatchingMethod.py
@@ -6,15 +6,39 @@ import numpy as np
 from Core.LotMatching.Contracts.LotMatchingMethod import LotMatchingMethod
 from Core.LotMatching.Schemas.Lot import Lot, LotAcquired, LotSold
 from Core.LotMatching.Schemas.Trade import Trade
+from Core.LotMatching.Services.TradeAssociationTracker import TradeAssociationTracker
+
+
+class TrackingTradeBuySellSide:
+    BuySide: Trade
+    SellSide: Trade
+
+    CandidateEvent: Trade
+    EarliestEventWaitingForMatch: Trade
+
+    def __init__(self, candidateEvent: Trade, earliestEventWaitingForMatch: Trade):
+        self.CandidateEvent = candidateEvent
+        self.EarliestEventWaitingForMatch = earliestEventWaitingForMatch
+
+        if earliestEventWaitingForMatch.Quantity < 0:
+            self.BuySide = candidateEvent
+            self.SellSide = earliestEventWaitingForMatch
+        else:
+            self.BuySide = earliestEventWaitingForMatch
+            self.SellSide = candidateEvent
+
+    def confirmConsumedQuantity(self, quantity: float, tracker: TradeAssociationTracker):
+        tracker.trackAcquiredQuantity(self.BuySide, quantity)
+        tracker.trackSoldQuantity(self.SellSide, quantity)
+
+    def getRemainingQuantity(self, of: Trade, tracker: TradeAssociationTracker):
+        if of == self.BuySide:
+            return of.Quantity - tracker.getAcquiredTradeTracker(of).Quantity
+        else:
+            return of.Quantity + tracker.getSoldTradeTracker(of).Quantity
 
 
 class FifoLotMatchingMethod(LotMatchingMethod):
-
-    class WorkingOnLot:
-        def __init__(self, lot: Lot):
-            self.lot = lot
-            self.remainingQuantity = lot.Quantity
-
     def performMatching(self, events: Sequence[Trade]) -> Sequence[Lot]:
 
         # Nothing to match with
@@ -26,9 +50,10 @@ class FifoLotMatchingMethod(LotMatchingMethod):
             return []
 
         # At this point we know that there are at least two events: one buy and one sell
+        sortedEvents = sorted(events, key=lambda x: x.Date)
 
         createdLots: list[Lot] = []
-        eventBacklog: deque[Trade] = deque(events)  # Standard Queue is missing type stubs, so using deque
+        eventBacklog: deque[Trade] = deque(sortedEvents)  # Standard Queue is missing type stubs, so using deque
         eventBacklog.reverse()
 
         eventsWaitingForMatchingEventsToMakeLot: deque[Trade] = deque()
@@ -37,31 +62,110 @@ class FifoLotMatchingMethod(LotMatchingMethod):
         while len(eventBacklog) > 0:
             newCandidateEvent = eventBacklog.pop()
 
-            earlistEventWaitingForMatch = eventsWaitingForMatchingEventsToMakeLot[-1]
+            earliestEventWaitingForMatch = eventsWaitingForMatchingEventsToMakeLot[-1]
+
+            # The most common case is when the quantities don't cancel out
+            # And the sign of the quantities are the same
+            # This means we just add it onto the events waiting for a match
+            if np.sign(earliestEventWaitingForMatch.Quantity) == np.sign(newCandidateEvent.Quantity):
+                eventsWaitingForMatchingEventsToMakeLot.appendleft(newCandidateEvent)
+                continue
+
+            trackingTradeBuySellSide = TrackingTradeBuySellSide(
+                candidateEvent=newCandidateEvent, earliestEventWaitingForMatch=earliestEventWaitingForMatch
+            )
+
+            self.tradeAssociationTracker.trackTrade(trackingTradeBuySellSide.BuySide)
+            self.tradeAssociationTracker.trackTrade(trackingTradeBuySellSide.SellSide)
+
+            if trackingTradeBuySellSide.BuySide.ID == trackingTradeBuySellSide.CandidateEvent.ID:
+                newCandidateEventQuantity = (
+                    trackingTradeBuySellSide.CandidateEvent.Quantity
+                    - self.tradeAssociationTracker.getAcquiredTradeTracker(trackingTradeBuySellSide.CandidateEvent).Quantity
+                )
+                earliestEventWaitingForMatchQuantity = (
+                    trackingTradeBuySellSide.EarliestEventWaitingForMatch.Quantity
+                    - self.tradeAssociationTracker.getSoldTradeTracker(trackingTradeBuySellSide.EarliestEventWaitingForMatch).Quantity
+                )
+            else:
+                earliestEventWaitingForMatchQuantity = (
+                    trackingTradeBuySellSide.EarliestEventWaitingForMatch.Quantity
+                    - self.tradeAssociationTracker.getAcquiredTradeTracker(trackingTradeBuySellSide.EarliestEventWaitingForMatch).Quantity
+                )
+                newCandidateEventQuantity = (
+                    trackingTradeBuySellSide.CandidateEvent.Quantity
+                    - self.tradeAssociationTracker.getSoldTradeTracker(trackingTradeBuySellSide.CandidateEvent).Quantity
+                )
+
+            # At this point we know we'll be consuming the earliest event waiting for a match
+            eventsWaitingForMatchingEventsToMakeLot.pop()
 
             # Most common and easiest case is when the quantities cancel out
-            doQuantitiesMatch = earlistEventWaitingForMatch.Quantity + newCandidateEvent.Quantity == 0
+            doQuantitiesMatch = earliestEventWaitingForMatchQuantity + newCandidateEventQuantity == 0
             if doQuantitiesMatch:
-                startingEventOfLot = eventsWaitingForMatchingEventsToMakeLot.pop()
-                endingEventOfLot = newCandidateEvent
-                quantityOfLot = startingEventOfLot.Quantity  # Since they're the same, we can just use the quantity of the starting event
+                quantityOfLot = abs(newCandidateEventQuantity)  # Since they're the same, we can just use the quantity of the starting event
 
-                buySideOfLot: LotAcquired = LotAcquired(Date=startingEventOfLot.Date, Relation=startingEventOfLot)
-                sellSideOfLot: LotSold = LotSold(Date=endingEventOfLot.Date, Relation=endingEventOfLot)
+                trackingTradeBuySellSide.confirmConsumedQuantity(quantityOfLot, self.tradeAssociationTracker)
+                buySideTrade = trackingTradeBuySellSide.BuySide
+                sellSideTrade = trackingTradeBuySellSide.SellSide
+
+                buySideOfLot: LotAcquired = LotAcquired(Date=buySideTrade.Date, Relation=buySideTrade)
+                sellSideOfLot: LotSold = LotSold(Date=sellSideTrade.Date, Relation=sellSideTrade)
 
                 createdLots.append(Lot(Quantity=quantityOfLot, Acquired=buySideOfLot, Sold=sellSideOfLot))
                 continue
 
-            # Next most common case is when the quantities don't cancel out
-            # And the sign of the quantities are the same
-            # This means we just add it onto the events waiting for a match
-            if np.sign(earlistEventWaitingForMatch.Quantity) == np.sign(newCandidateEvent.Quantity):
-                eventsWaitingForMatchingEventsToMakeLot.appendleft(newCandidateEvent)
-                continue
-
-            # If the quantities don't cancel out and the signs are different, we need to make a lot
+            # If the quantities don't cancel out and the signs are different, we need to generate Lots
             # Since we know the quantities don't cancel out, we need to consume all the events waiting for a match
-            # Until we've exhausted either the newCandidateEvent or the events waiting for a match
+            # we do this until we've exhausted either the newCandidateEvent or the events waiting for a match
+            # At which point, we either put the remainder of the newCandidateEvent back into the event backlog
+            # (to be the first one to be matched in the next iteration)
+            # or we push the remainder of the events waiting for a match onto the event backlog (switching position from long to short or short to long)
+
+            quantityAvailableToConsumeFromNewCandidateEvent = trackingTradeBuySellSide.getRemainingQuantity(
+                trackingTradeBuySellSide.CandidateEvent, self.tradeAssociationTracker
+            )
+            quantityAvailableToConsumeFromEarliestEventWaitingForMatch = trackingTradeBuySellSide.getRemainingQuantity(
+                trackingTradeBuySellSide.EarliestEventWaitingForMatch, self.tradeAssociationTracker
+            )
+
+            if (
+                abs(quantityAvailableToConsumeFromNewCandidateEvent) > 0
+                and abs(quantityAvailableToConsumeFromEarliestEventWaitingForMatch) > 0
+            ):
+
+                willConsumeQuantity = min(
+                    abs(quantityAvailableToConsumeFromNewCandidateEvent), abs(quantityAvailableToConsumeFromEarliestEventWaitingForMatch)
+                )
+
+                trackingTradeBuySellSide.confirmConsumedQuantity(willConsumeQuantity, self.tradeAssociationTracker)
+
+                acquired = LotAcquired(Date=earliestEventWaitingForMatch.Date, Relation=earliestEventWaitingForMatch)
+                sold = LotSold(Date=newCandidateEvent.Date, Relation=newCandidateEvent)
+                createdLots.append(Lot(Quantity=willConsumeQuantity, Acquired=acquired, Sold=sold))
+
+                quantityAvailableToConsumeFromNewCandidateEvent = trackingTradeBuySellSide.getRemainingQuantity(
+                    trackingTradeBuySellSide.CandidateEvent, self.tradeAssociationTracker
+                )
+                quantityAvailableToConsumeFromEarliestEventWaitingForMatch = trackingTradeBuySellSide.getRemainingQuantity(
+                    trackingTradeBuySellSide.EarliestEventWaitingForMatch, self.tradeAssociationTracker
+                )
+
+                # If we've consumed the earliest event waiting for a match,
+                # and we've yet to fully process the new candidate event,
+                # we can just put the new candidate back into queue for it to be processed again
+                if (
+                    quantityAvailableToConsumeFromEarliestEventWaitingForMatch == 0
+                    and len(eventsWaitingForMatchingEventsToMakeLot) > 0
+                    and quantityAvailableToConsumeFromNewCandidateEvent != 0
+                ):
+                    eventBacklog.append(trackingTradeBuySellSide.CandidateEvent)
+
+                # However, if we've consumed the new candidate event,
+                # but haven't fully consumed the earliest event waiting for a match,
+                # we should put it back into the queue for it to be processed again
+                if quantityAvailableToConsumeFromNewCandidateEvent == 0 and quantityAvailableToConsumeFromEarliestEventWaitingForMatch != 0:
+                    eventsWaitingForMatchingEventsToMakeLot.append(earliestEventWaitingForMatch)
 
         return createdLots
 
--- src/Core/LotMatching/Services/LotMatchingMethods/Tests/TestFifoLotMatchingMethod.py
+++ src/Core/LotMatching/Services/LotMatchingMethods/Tests/TestFifoLotMatchingMethod.py
@@ -18,6 +18,42 @@ simpleCaseNoProfit = {
     ),
 }
 
+simpleCaseWithMultipleSellEvents = {
+    "Buy": Trade.Trade(
+        ID="ID",
+        Quantity=2,
+        Date=get("2023-01-01"),
+    ),
+    "Sell": Trade.Trade(
+        ID="ID2",
+        Quantity=-1,
+        Date=get("2023-01-02"),
+    ),
+    "Sell2": Trade.Trade(
+        ID="ID3",
+        Quantity=-1,
+        Date=get("2023-01-03"),
+    ),
+}
+
+simpleCaseWithMultipleBuyEvents = {
+    "Buy": Trade.Trade(
+        ID="ID",
+        Quantity=1,
+        Date=get("2023-01-01"),
+    ),
+    "Buy2": Trade.Trade(
+        ID="ID2",
+        Quantity=1,
+        Date=get("2023-01-02"),
+    ),
+    "Sell": Trade.Trade(
+        ID="ID3",
+        Quantity=-2,
+        Date=get("2023-01-02"),
+    ),
+}
+
 
 class TestFifoLotMatchingMethod:
 
@@ -36,3 +72,59 @@ class TestFifoLotMatchingMethod:
         assert (
             lots[0].Sold.Relation.Date == simpleCaseNoProfit["Sell"].Date
         ), "Generated lot's acquisition date should match the predefined lot's"
+
+    def testMultipleBuyEvents(self):
+        method = FifoLotMatchingMethod()
+
+        events = [simpleCaseWithMultipleBuyEvents["Buy"], simpleCaseWithMultipleBuyEvents["Buy2"], simpleCaseWithMultipleBuyEvents["Sell"]]
+        lots = method.performMatching(events)
+
+        assert len(lots) == 2, "Given multiple buy events, a lot should be generated for each buy event"
+
+        assert (
+            lots[0].Quantity == simpleCaseWithMultipleBuyEvents["Buy"].Quantity
+        ), "Generated Lot Quantity should match the first buy event's"
+        assert (
+            lots[0].Acquired.Relation.ID == simpleCaseWithMultipleBuyEvents["Buy"].ID
+        ), "Generated lot's acquisition should point to the first buy event"
+        assert lots[0].Sold.Relation.ID == simpleCaseWithMultipleBuyEvents["Sell"].ID, "Generated lot's sale should point to the sell event"
+
+        assert (
+            lots[1].Quantity == simpleCaseWithMultipleBuyEvents["Buy2"].Quantity
+        ), "Generated Lot Quantity should match the second buy event's"
+        assert (
+            lots[1].Acquired.Relation.ID == simpleCaseWithMultipleBuyEvents["Buy2"].ID
+        ), "Generated lot's acquisition should point to the second buy event"
+        assert lots[1].Sold.Relation.ID == simpleCaseWithMultipleBuyEvents["Sell"].ID, "Generated lot's sale should point to the sell event"
+
+    def testMultipleSellEvents(self):
+        method = FifoLotMatchingMethod()
+
+        events = [
+            simpleCaseWithMultipleSellEvents["Buy"],
+            simpleCaseWithMultipleSellEvents["Sell"],
+            simpleCaseWithMultipleSellEvents["Sell2"],
+        ]
+        lots = method.performMatching(events)
+
+        assert len(lots) == 2, "Given multiple sell events, a lot should be generated for each sell event"
+
+        assert lots[0].Quantity == abs(
+            simpleCaseWithMultipleSellEvents["Sell"].Quantity
+        ), "Generated Lot Quantity should match the first sell event's"
+        assert (
+            lots[0].Acquired.Relation.ID == simpleCaseWithMultipleSellEvents["Buy"].ID
+        ), "Generated lot's acquisition should point to the first buy event"
+        assert (
+            lots[0].Sold.Relation.ID == simpleCaseWithMultipleSellEvents["Sell"].ID
+        ), "Generated lot's sale should point to the sell event"
+
+        assert lots[1].Quantity == abs(
+            simpleCaseWithMultipleSellEvents["Sell2"].Quantity
+        ), "Generated Lot Quantity should match the second sell event's"
+        assert (
+            lots[1].Acquired.Relation.ID == simpleCaseWithMultipleSellEvents["Buy"].ID
+        ), "Generated lot's acquisition should point to the first buy event"
+        assert (
+            lots[1].Sold.Relation.ID == simpleCaseWithMultipleSellEvents["Sell2"].ID
+        ), "Generated lot's sale should point to the second sell event"
