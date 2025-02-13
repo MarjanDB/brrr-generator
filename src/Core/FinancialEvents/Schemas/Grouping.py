ee0f674a2ab33f84c12aedc3ad04f7a254f6ecfe
MarjanDB <mocnik.mario@hotmail.com>
2025-02-13 23:01:56 +0100

    add name as possible identifier as well

--- src/Core/FinancialEvents/Schemas/Grouping.py
+++ src/Core/FinancialEvents/Schemas/Grouping.py
@@ -14,9 +14,10 @@ from Core.FinancialEvents.Schemas.Lots import TaxLotDerivative, TaxLotStock
 
 
 class FinancialGroupingIdentifier:
-    def __init__(self, ISIN: str | None = None, Ticker: str | None = None):
+    def __init__(self, ISIN: str | None = None, Ticker: str | None = None, Name: str | None = None):
         self._Isin = ISIN
         self._Ticker = Ticker
+        self._Name = Name
 
     def setIsin(self, ISIN: str | None):
         self._Isin = ISIN
@@ -24,18 +25,24 @@ class FinancialGroupingIdentifier:
     def setTicker(self, Ticker: str | None):
         self._Ticker = Ticker
 
+    def setName(self, Name: str | None):
+        self._Name = Name
+
     def getIsin(self) -> str | None:
         return self._Isin
 
     def getTicker(self) -> str | None:
         return self._Ticker
 
+    def getName(self) -> str | None:
+        return self._Name
+
     def isTheSameAs(self, other: Self) -> bool:
-        return self._Isin == other._Isin or self._Ticker == other._Ticker
+        return self._Isin == other._Isin or self._Ticker == other._Ticker or self._Name == other._Name
 
     @staticmethod
     def fromStagingIdentifier(identifier: sfg.StagingFinancialGroupingIdentifier) -> "FinancialGroupingIdentifier":
-        return FinancialGroupingIdentifier(ISIN=identifier.getIsin(), Ticker=identifier.getTicker())
+        return FinancialGroupingIdentifier(ISIN=identifier.getIsin(), Ticker=identifier.getTicker(), Name=identifier.getName())
 
 
 @dataclass
--- src/Core/StagingFinancialEvents/Schemas/Grouping.py
+++ src/Core/StagingFinancialEvents/Schemas/Grouping.py
@@ -11,9 +11,10 @@ from Core.StagingFinancialEvents.Schemas.Lots import StagingTaxLot
 
 
 class StagingFinancialGroupingIdentifier:
-    def __init__(self, ISIN: str | None = None, Ticker: str | None = None):
+    def __init__(self, ISIN: str | None = None, Ticker: str | None = None, Name: str | None = None):
         self._Isin = ISIN
         self._Ticker = Ticker
+        self._Name = Name
 
     def setIsin(self, ISIN: str | None):
         self._Isin = ISIN
@@ -21,14 +22,20 @@ class StagingFinancialGroupingIdentifier:
     def setTicker(self, Ticker: str | None):
         self._Ticker = Ticker
 
+    def setName(self, Name: str | None):
+        self._Name = Name
+
     def getIsin(self) -> str | None:
         return self._Isin
 
     def getTicker(self) -> str | None:
         return self._Ticker
 
+    def getName(self) -> str | None:
+        return self._Name
+
     def isTheSameAs(self, other: Self) -> bool:
-        return self._Isin == other._Isin or self._Ticker == other._Ticker
+        return self._Isin == other._Isin or self._Ticker == other._Ticker or self._Name == other._Name
 
 
 @dataclass
